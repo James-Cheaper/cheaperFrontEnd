8ca458d6c742788f063a42f3eae282b31cc562df
James Cacapit <jamescacapit@gmail.com>
2025-05-10 15:04:26 -0700

    postbuild

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -5,6 +5,21 @@ const path = require('path');
 const indexPath = path.join(__dirname, '../build/index.html');
 let indexHtml = fs.readFileSync(indexPath, 'utf8');
 
+
+
+const jsonData = fs.readFileSync('path/to/somefile.json', 'utf-8');
+
+try {
+  const data = JSON.parse(jsonData);
+  console.log('JSON data parsed successfully:', data);
+} catch (error) {
+  console.error('Failed to parse JSON:', error.message);
+  console.error('File Content:', jsonData); // See what was inside
+  process.exit(1); // Exit with an error
+}
+
+
+
 indexHtml = indexHtml.replace(/\/static\//g, './static/');
 indexHtml = indexHtml.replace(/\/favicon\.ico/g, './favicon.ico');
 indexHtml = indexHtml.replace(/\/logo\d+\.png/g, (match) => `./${match.substring(1)}`);
