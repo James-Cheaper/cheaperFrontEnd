1057a242172144334ca47cb5547bab9f6f07aaea
James Cacapit <jamescacapit@gmail.com>
2025-05-21 18:00:48 -0700

    eslintrc and removed type:module

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  parser: "@babel/eslint-parser",
+  parserOptions: {
+    requireConfigFile: false,
+    babelOptions: {
+      presets: ["@babel/preset-react"],
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    jest: true,
+  },
+  plugins: ["react", "react-hooks", "jsx-a11y"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+  ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    "no-console": ["warn", { allow: ["warn", "error"] }],
+    eqeqeq: ["error", "always"],
+    "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "react/prop-types": "off",
+    "react/react-in-jsx-scope": "off",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+  },
+};
